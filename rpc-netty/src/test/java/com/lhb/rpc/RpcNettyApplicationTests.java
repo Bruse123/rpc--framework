81a279e9e75ac779d31257185a71e012b397e320
Bruse123 <1340705062@qq.com>
2021-02-03 00:10:01 +0800

    创建rpc-api

---
+++ rpc-netty/src/test/java/com/lhb/rpc/RpcNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhb.rpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
